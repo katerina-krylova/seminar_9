335f17608425ce7b883705e714bdae86cc8b3a52
Katerina Krylova <katerina-zaytseva@yandex.com>
2022-09-21 15:41:22 +0300

    Решение к задаче 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+void PrintInterval(int m, int n)
+		{
+			int start = m;
+			int end = n;
+			if(m > n)
+			{
+				start = n;
+				end = m;
+			}
+			for(int i = start; i <= end; i++)
+			{
+				Console.Write($"{i} ");
+			}
+		}
+		PrintInterval(22, 9);
+		
+		Console.WriteLine();
