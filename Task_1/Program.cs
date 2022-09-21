e739fc634d37c72dfdea85a976f74104e7b34dc5
Katerina Krylova <katerina-zaytseva@yandex.com>
2022-09-21 15:41:05 +0300

    Решение к задаче 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать натуральные числа от N до 1, N задано
+int n = InputNumbers("Введите n: ");
+int count = 2;
+PrintNumber(n, count);
+Console.Write(1);
+
+void PrintNumber(int n, int count)
+{
+  if (count > n) return;
+  PrintNumber(n, count + 1);
+  Console.Write(count + ", ");
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
